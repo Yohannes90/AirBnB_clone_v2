616797d5f81f8cfbb99cdebb33ea2132ae8758b0
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-08 22:41:00 +0300

    first commit for question 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" deploy or distribute an archive to a web servers"""
+import os.path
+from fabric.api import put, run, env
+
+env.hosts = ["34.207.63.58", "3.90.81.96"]
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to a web servers
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        True if all operations have been done correctly, otherwise False
+    """
+
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
