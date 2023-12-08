a5caeda18864867dcaa51f89ee2cf2fa4656fc0c
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-08 22:25:47 +0300

    first commit for question 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""generate a .tgz archive from contents of the web_static in AirBnB repo"""
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
