44a3eb7f2175b57b233b8a1ae7ad4f94406501cb
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-01 23:35:20 +0300

    updating models/city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
 import models
 from models.base_model import BaseModel
 from os import getenv
@@ -12,7 +13,7 @@ class City(BaseModel):
     """Representation of city """
     if models.storage_t == "db":
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
     else:
